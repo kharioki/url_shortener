834468e0bbd76b91ff304a22701007cb86576f90
Tony Kharioki <antolisah@gmail.com>
2024-07-29 14:57:42 +0300

    created link model

---
+++ db/migrate/20240729105223_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :description
+      t.string :image
+      t.integer :views_count
+
+      t.timestamps
+    end
+  end
+end
